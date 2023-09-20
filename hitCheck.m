1b18551768b2f66a6658de50fbef05a841872f21
NitzanAlbeck <48420536+NitzanAlbeck@users.noreply.github.com>
2023-09-20 17:37:53 +0300

    Update hitCheck.m
    Checking the timestamps from the OE that are nearest to the hits

--- hitCheck.m
+++ hitCheck.m
@@ -1,9 +1,21 @@
-function is_hit = hitCheck(trig_map_csv_path, events_csv_path)
+function is_hit = hitCheck(trig_map, events_csv_path)
+
+% this function...
+% Inputs:
+% * trig_map_csv_path / trig_map -
+%   can get either the CSV path file or the mat that is the uotput of
+%   getTriggerMap.
 
 % Getting triggers timestamps and events
-trig_map = readmatrix(trig_map_csv_path);
-E = readtable(events_csv_path, 'Delimiter', ',', 'ReadVariableNames', true);
+if isa(trig_map,"string") % if the trig_map is a path to the CSV:
+    trig_map = readmatrix(trig_map_csv_path);
+    disp ("the variable is a path, taking data form the CSV")
+elseif isa(trig_map,"double") % if its the mat
+    disp ("the trig_map is a mat")
+end 
 
+% reading the event file of the trial:
+E = readtable(events_csv_path, 'Delimiter', ',', 'ReadVariableNames', true);
 % Selecting only the hits timestamp
 E = E(strcmp(E.event, 'screen_touch'),:);
 E_hit = contains(E.value, '"is_hit": true');
@@ -11,7 +23,7 @@ hit_positions = E_hit==1;
 E_hit_timestamp = table2array(E(hit_positions,"time")); 
 
 % Matching the hit to the nearest trigger
-B = trig_map(:,1);
+B = trig_map(:,1); % taking the video triggers timestamps from behavioral comp
 result = false(size(B));
 for i = 1:numel(B) - 1
     if any (E_hit_timestamp > B(i) & E_hit_timestamp < B(i+1))
